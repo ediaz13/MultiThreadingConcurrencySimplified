02bce2704718d471de5ca1d8222c7b8987372ee1
ediaz13 <diazemanuel27@gmail.com>
2024-03-20 22:00:25 -0300

    Thread Termination & Daemon Threads

---
+++ MultiThreading/src/main/java/org/mogul/interrupt/MainLongComputation.java
@@ -0,0 +1,41 @@
+package org.mogul.interrupt;
+
+import java.math.BigInteger;
+
+public class MainLongComputation {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new LongComputationalTask(new BigInteger("20000000"), new BigInteger("4555555555")));
+        thread.start();
+        thread.interrupt();
+    }
+
+    
+
+    private static class LongComputationalTask implements Runnable {
+        private final BigInteger base;
+        private final BigInteger power;
+
+        public LongComputationalTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Prematurely Interrupted Computation");
+                }
+                result = result.multiply(base);
+            }
+
+            return result;
+        }
+    }
+}
