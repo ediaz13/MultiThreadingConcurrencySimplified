f5806923ab913bd4b82782f9cca12ebec2a7a7ff
ediaz13 <diazemanuel27@gmail.com>
2024-11-06 09:46:12 -0300

    Threads Creation.

--- MultiThreading/src/main/java/org/mogul/section2/example1/Main.java
+++ MultiThreading/src/main/java/org/mogul/section2/example1/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         System.out.println("We are now in thread: " +  Thread.currentThread().getName() + " before starting a new thread");
         thread.start();
-        System.out.println("We are now in thread: " +  Thread.currentThread().getName() + " before starting a new thread");
+        System.out.println("We are now in thread: " +  Thread.currentThread().getName() + " after starting a new thread");
 
         thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
